671b70eaaef64c64b60fbb4c4491ac5edfdbffe2
Brent Shaffer <bshafs@gmail.com>
2012-07-13 13:44:53 -0600

    adds reportTrended and Overtime

--- lib/AdobeDigitalMarketing/Api/Report.php
+++ lib/AdobeDigitalMarketing/Api/Report.php
@@ -10,8 +10,32 @@
 class AdobeDigitalMarketing_Api_Report extends AdobeDigitalMarketing_Api
 {
     /**
-     * Submits a Ranked report request. Ranked reports display the rankings of the report pages in relation to the metric
+     * Submits a Ranked report request. Ranked reports display the rankings of the report pages in relation to the metric.
+     * Ranked reports can display multiple metrics in a report.
+     * Ranked reports have the following characteristics:
+     * 
+     *  * Display the rankings of the report pages in relation to the metric.
+     *  * Display multiple metrics in a report, if desired.
+     *  * Support multiple elements in a report, if desired.
+     *
      * https://developer.omniture.com/en_US/documentation/sitecatalyst-reporting/r-queueranked
+     *
+     * @param   array $reportDescription  the report description array
+     * @return  array 
+     *   - reportID: The report ID of the queued report
+     */
+    public function queueRanked($reportDescription)
+    {
+        $response = $this->post('Report.QueueRanked', array(
+            'reportDescription' => $reportDescription,
+        ));
+
+        return $this->returnResponse($response);
+    }
+    
+    /**
+     * Submits a Trended report request. Trended reports display trends for a single metric (revenue, orders, views, etc) and element (product, category, page, etc).
+     * https://developer.omniture.com/en_US/documentation/sitecatalyst-reporting/r-queuetrended
      *
      * @param   array $reportDescription  the report description array
      * @return  array 
@@ -19,7 +43,25 @@ class AdobeDigitalMarketing_Api_Report extends AdobeDigitalMarketing_Api
      */
     public function queueTrended($reportDescription)
     {
-        $response = $this->post('Report.QueueRanked', array(
+        $response = $this->post('Report.QueueTrended', array(
+            'reportDescription' => $reportDescription,
+        ));
+
+        return $this->returnResponse($response);
+    }
+
+    /**
+     * Submits an Overtime report request. Overtime reports display the specified metrics over a defined time period. 
+     * Overtime reports can display multiple metrics in a report. The only possible element in an Overtime report is time.
+     * https://developer.omniture.com/en_US/documentation/sitecatalyst-reporting/r-queuetrended
+     *
+     * @param   array $reportDescription  the report description array
+     * @return  array 
+     *   - reportID: The report ID of the queued report
+     */
+    public function queueOvertime($reportDescription)
+    {
+        $response = $this->post('Report.QueueOvertime', array(
             'reportDescription' => $reportDescription,
         ));
 
